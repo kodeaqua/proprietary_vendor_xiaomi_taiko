c01327e0ad567a6623286f4a2a533e6bbcc239df
ItzJustNett <hinhab1@gmail.com>
2026-07-25 21:39:08 +0300

    taiko: proprietary vendor blobs for CrDroid 16.0
    Generated with extract-files.py from stock dump; includes MTK camera stack
    (camerahalserver, camera.provider@2.6-impl, GC08A8/GC05A2 sensors, mtkcam
    pipeline) and MTK Bluetooth (libbt-vendor, libbluetooth_mtk).

---
+++ Android.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),taiko)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,bfe4967971d0e8056c3c88caad84e435dbbda6a5)
+$(call add-radio-file-sha1-checked,radio/gz.img,abe2e593cff86cb62a6e5a44ab115d7b1b287222)
+$(call add-radio-file-sha1-checked,radio/lk.img,f27c8e85ec7e6cdbc14a36160eefd01db29c60b6)
+$(call add-radio-file-sha1-checked,radio/logo.img,d72e034e87bcddded8c87add204dd03b9b0c1674)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,7adf45b20c95c881e0377890c6e4dca9d7c100f8)
+$(call add-radio-file-sha1-checked,radio/md1img.img,ff2af88633909fb812a5646db8049f57b5045db6)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,088a3e16591bdfd5b36ecb44492dd0605eedcc76)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,2fcf3fb3ac5423940c3f3a9e95d9cb96cbdccbd2)
+$(call add-radio-file-sha1-checked,radio/scp.img,6dbd3782b42928c5983b91dd08b861db9d501964)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,29f6cf88c01b0a98040f3237160a1dde1ec2d497)
+$(call add-radio-file-sha1-checked,radio/sspm.img,7b58cf8b430fbc099f79a538e94d0ba7ed8c25e8)
+$(call add-radio-file-sha1-checked,radio/tee.img,da4222bd4a2d6b9e1a3c20c83de226210c36a081)
+
+endif
